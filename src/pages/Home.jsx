0c063bdb9be9155a042447942edaa8d227ff8499
harshakl03 <harshaharsha87593@gmail.com>
2024-02-09 20:38:19 +0530

    test

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,11 @@
-import { useSelector } from "react-redux";
 import Input from "../ui/Input";
 import Button from "../ui/Button";
 import Label from "../ui/Label";
 
 export default function Home() {
-  const { name: nameA } = useSelector((state) => state.partA);
-  const { name: nameB } = useSelector((state) => state.partB);
-  const { name: nameC } = useSelector((state) => state.partC);
-  
   return (
     <div>
-      Home {nameA + nameB + nameC}
+      Home
       <div>
         <Input />
         <br />
@@ -23,7 +18,6 @@ export default function Home() {
       <Button to="/login">Log In</Button>
       <Button to="/register">Register</Button>
       <Button>Normal</Button>
-      
     </div>
   );
 }

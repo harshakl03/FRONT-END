4090a28ec935a0663d0e54fd660c2fbb10b730b7
surajar36 <arsuraj381@gmail.com>
2024-02-14 19:30:53 +0530

    Changes in header and footer

--- src/ui/Stylers.jsx
+++ src/ui/Stylers.jsx
@@ -5,13 +5,15 @@ export const Header = styled.header`
   align-items: center;
   justify-content: center;
   margin-top: 20px;
-  font-size: 30px;
+  font-size: 24px;
   text-decoration: underline;
+  
 `;
 
 export const Footer = styled.footer`
   display: flex;
   align-items: center;
-  opacity: 50%;
+  opacity: 40%;
   margin: 20px;
+  text-align: end;
 `;
